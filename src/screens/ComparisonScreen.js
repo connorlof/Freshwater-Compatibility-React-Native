1e84896ecd01ace40bc48d74c96bb8af1397402a
Connor Loftus <connorlof@gmail.com>
2020-01-12 20:46:19 -0500

    clear functionality and adding static fish to comparison array

--- src/screens/ComparisonScreen.js
+++ src/screens/ComparisonScreen.js
@@ -4,23 +4,47 @@ import comparisonData from '../../assets/data/family_comparison_freshwater.json'
 import ModalTester from '../components/ModalTester.js';
 
 const ComparisonScreen = () => {
-    const [addedFish, setAddedFish] = useState([]);
-    const [result, setResult] = useState('');
+    const addedFish = [];
+    const [result, setResult] = useState(['']);
 
-    const onFishAdded = (fishAdded) => {
-        console.log('Added fish: ', fishAdded);
-        setAddedFish([fishAdded]);
-        console.log('addedFish index 0: ', addedFish[0]);
+    const clearFish = () => {
+        addedFish.length = 0;
+        console.log('addedFish: ', addedFish);
+
+        determineResult(addedFish);
+    }
+
+    const addFish = (fishToAdd) => {
+        console.log('Added fish: ', fishToAdd);
+        addedFish.push(fishToAdd);
+        console.log('addedFish: ', addedFish);
+
+        determineResult(addedFish);
+    }
+
+    const determineResult = (addedFish) => {
+        const resultsString = addedFish.toString();
+        console.log(resultsString);
+
+        //setResult(resultsString);
     }
 
     return (
         <View>
             <Text>Freshwater Compatibility</Text>
             
+            <Text>{result}</Text>
+
+            <TouchableOpacity onPress={() => addFish("Angelfish")}>
+                <Text style={styles.buttonText}>Add angelfish</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => addFish("Bettas")}>
+                <Text style={styles.buttonText}>Add betta</Text>
+            </TouchableOpacity>
             <TouchableOpacity>
                 <ModalTester/>
             </TouchableOpacity>
-            <TouchableOpacity onPress={() => onFishAdded("Angelfish")}>
+            <TouchableOpacity onPress={() => clearFish()}>
                 <Text style={styles.buttonText}>Clear Tank</Text>
             </TouchableOpacity>
         </View>
